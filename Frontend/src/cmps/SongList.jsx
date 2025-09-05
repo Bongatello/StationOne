3903b8ac9fb8ed516e86b8bb3ce68db2c64aacce
Timur H. <bongatelloninja@gmail.com>
2025-09-05 16:29:33 +0300

    -Playing song is now styled (Green title and index), has toggle play/pause button to the song itself
    -Non playing songs inside station have play button, and upon press, the player plays that song

--- Frontend/src/cmps/SongList.jsx
+++ Frontend/src/cmps/SongList.jsx
@@ -2,8 +2,13 @@
 import { togglePlayerState, getPlayingSong, setPlayingSong } from '../store/player.actions'
 import { getDuration } from "../services/util.service";
 import { findOnYoutube } from '../services/songs/songs.service.js';
+import { useSelector } from 'react-redux';
 export function SongList ({song, index}){
+    const playerData = useSelector(state => state.playerModule.player)
     
+    const playButton = <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606"/>
+    const pauseButton = <path d="M5.7 3a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7zm10 0a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7z"/>
+
     function timeAddedAgo(){
         const currentTime = Date.now()
         const timeSinceAdded = currentTime - song.dateAdded
@@ -20,12 +25,20 @@ export function SongList ({song, index}){
 
     return(
         <div className="song-preview-container">
-            <div className="song-preview">
-                <div className="song-index" onClick={() => {setPlayingSong(song)
-                    findOnYoutube(song)
+            <div className={song._id === playerData.currentSong._id ? "playing-song-preview song-preview": "song-preview"}>
+                <div className="song-index" onClick={() => {
+                    if (!(song._id === playerData.currentSong._id)){
+                        setPlayingSong(song)
+                        findOnYoutube(song)
+                        togglePlayerState(true)
+                    }
+                    else {
+                        togglePlayerState(!playerData.isPlaying)
+                    }
                 }}>
                     <svg width='16px' height='16px' viewBox="0 0 24 24">
-                        <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606"/>
+                        {(!playerData.isPlaying || !(song._id === playerData.currentSong._id)) && playButton} {/* song is not playing OR song is not selected, show play button */} 
+                        {playerData.isPlaying && song._id === playerData.currentSong._id && pauseButton} {/* song is playing AND song is selected, show pause button */}
                     </svg>
                     <p>{index + 1}</p>
                 </div>
